b105cae78739353567d48ec2a9b3404efed4bb17
rahulg13 <rahulgargabc@gmail.com>
2020-07-16 23:18:03 +0530

    fileconnection and readlines usage done

--- 1DataCleaning.R
+++ 1DataCleaning.R
@@ -1,1 +1,17 @@
+rm(list = ls())
+
 #1. Data cleaning
+
+# 2 tasks - reading the data and sample it 
+
+#File connection
+con <- file("final/en_US/en_US.blogs.txt")
+
+#Connection Open
+open(con, "r")
+l <- length(readLines(con)) 
+# l for en_US.blogs is 899288
+input1 <- readLines(con, n = 5)
+
+#Connection Close, Any reading of data above it, all processing below it
+close(con)
