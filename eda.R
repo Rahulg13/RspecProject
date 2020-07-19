f66e90a663fe26c22b6a8f632db5be3928a253e2
rahulg13 <rahulgargabc@gmail.com>
2020-07-19 16:21:18 +0530

    Using tm

--- eda.R
+++ eda.R
@@ -9,3 +9,11 @@ con1 <- file("SampleENBlogs.txt")
 x <- termFreq(readLines(con1), control = list(tolower = TRUE, removePunctuation = TRUE, stemming = TRUE))
 y <- order(x, decreasing = TRUE)
 x <- x[y]
+
+
+# trying the tm package
+corpus1 <- VCorpus(VectorSource(readLines(con1)), readerControl = list(dbControl = TRUE))
+dtm1 <- DocumentTermMatrix(corpus1, control = list(dbControl = TRUE) )
+
+corpus2 <- Corpus(DirSource("data1/"), readerControl = list(reader = readPlain))
+dtm2 <- DocumentTermMatrix(corpus2)
