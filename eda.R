7df1fe839838097818c6733a7f6d325debceca01
rahulg13 <rahulgargabc@gmail.com>
2020-07-20 09:06:37 +0530

    Ngram from tm used successfully

--- eda.R
+++ eda.R
@@ -1,19 +1,21 @@
 # Exploratory Data Analysis
 library(tm)
 library(SnowballC)
+library(RWeka)
 
 #corpus1 <- Corpus(DirSource("SampleENBlogs.txt"), readerControl = readPlain)
 # address not a directory
 
+# method 1 : quicker one 
 con1 <- file("SampleENBlogs.txt")
 x <- termFreq(readLines(con1), control = list(tolower = TRUE, removePunctuation = TRUE, stemming = TRUE))
 y <- order(x, decreasing = TRUE)
 x <- x[y]
 
 
+# method 2 
 # trying the tm package
-corpus1 <- Corpus(VectorSource(readLines(con1)))
-close(con1)
+corpus1 <- VCorpus(VectorSource(readLines(con1)))
 corpus1 <- tm_map(corpus1, content_transformer(tolower))
 corpus1 <- tm_map(corpus1, FUN = removePunctuation)
 corpus1 <- tm_map(corpus1, FUN = removeWords, stopwords(kind = "en"))
@@ -21,15 +23,31 @@ corpus1 <- tm_map(corpus1, FUN = stemDocument)
 corpus1 <- tm_map(corpus1, FUN = stripWhitespace)
 
 
-dtm1 <- DocumentTermMatrix(corpus1)
-dtm <- removeSparseTerms(dtm, 0.998)
+# one-gram
+dtm <- DocumentTermMatrix(corpus1)
+dtm <- removeSparseTerms(dtm, 0.999)
 mat <- as.matrix(dtm)
-
-
 wordfreq <- colSums(mat)
-#shows all terms with frequency above 1000
-dtm[findFreqTerms(dtm3, 1000)]
+wordfreq <- sort(wordfreq, decreasing = TRUE)
 
 
-corpus2 <- Corpus(DirSource("data1/"), readerControl = list(reader = readPlain))
-dtm2 <- DocumentTermMatrix(corpus2)
+# bi-gram processing
+tokenarg <- function(x) { NGramTokenizer(x,Weka_control(min = 2, max = 2)) } 
+dtm2 <- DocumentTermMatrix(corpus1, control = list(tokenize = tokenarg))
+dtm2 <- removeSparseTerms(dtm2, 0.999)
+mat2 <- as.matrix(dtm2)
+wordfreq2 <- colSums(mat2)
+wordfreq2 <- sort(wordfreq2, decreasing = TRUE)
+
+
+# tri-gram processing
+tokenarg <- function(x) { NGramTokenizer(x,Weka_control(min = 3, max = 3)) } 
+dtm3 <- DocumentTermMatrix(corpus1, control = list(tokenize = tokenarg))
+dtm3 <- removeSparseTerms(dtm3, 0.9999)
+mat3 <- as.matrix(dtm3)
+wordfreq3 <- colSums(mat3)
+wordfreq3 <- sort(wordfreq3, decreasing = TRUE)
+
+
+
+
