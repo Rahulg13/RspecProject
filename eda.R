50f414e5e585b3546304b5357455a3b211eab9f6
rahulg13 <rahulgargabc@gmail.com>
2020-07-21 11:45:02 +0530

    Some improvements

--- eda.R
+++ eda.R
@@ -8,9 +8,9 @@ library(RWeka)
 
 # method 1 : quicker one 
 con1 <- file("SampleENBlogs.txt")
-x <- termFreq(readLines(con1), control = list(tolower = TRUE, removePunctuation = TRUE, stemming = TRUE))
-y <- order(x, decreasing = TRUE)
-x <- x[y]
+#x <- termFreq(readLines(con1), control = list(tolower = TRUE, removePunctuation = TRUE, stemming = TRUE))
+#y <- order(x, decreasing = TRUE)
+#x <- x[y]
 
 
 # method 2 
@@ -19,13 +19,13 @@ corpus1 <- VCorpus(VectorSource(readLines(con1)))
 corpus1 <- tm_map(corpus1, content_transformer(tolower))
 corpus1 <- tm_map(corpus1, FUN = removePunctuation)
 corpus1 <- tm_map(corpus1, FUN = removeWords, stopwords(kind = "en"))
-corpus1 <- tm_map(corpus1, FUN = stemDocument)
+#corpus1 <- tm_map(corpus1, FUN = stemDocument)
 corpus1 <- tm_map(corpus1, FUN = stripWhitespace)
 
 
 # one-gram
 dtm <- DocumentTermMatrix(corpus1)
-dtm <- removeSparseTerms(dtm, 0.999)
+dtm <- removeSparseTerms(dtm, 0.9999)
 mat <- as.matrix(dtm)
 wordfreq <- colSums(mat)
 wordfreq <- sort(wordfreq, decreasing = TRUE)
@@ -34,7 +34,7 @@ wordfreq <- sort(wordfreq, decreasing = TRUE)
 # bi-gram processing
 tokenarg <- function(x) { NGramTokenizer(x,Weka_control(min = 2, max = 2)) } 
 dtm2 <- DocumentTermMatrix(corpus1, control = list(tokenize = tokenarg))
-dtm2 <- removeSparseTerms(dtm2, 0.999)
+dtm2 <- removeSparseTerms(dtm2, 0.9999)
 mat2 <- as.matrix(dtm2)
 wordfreq2 <- colSums(mat2)
 wordfreq2 <- sort(wordfreq2, decreasing = TRUE)
@@ -43,7 +43,7 @@ wordfreq2 <- sort(wordfreq2, decreasing = TRUE)
 # tri-gram processing
 tokenarg <- function(x) { NGramTokenizer(x,Weka_control(min = 3, max = 3)) } 
 dtm3 <- DocumentTermMatrix(corpus1, control = list(tokenize = tokenarg))
-dtm3 <- removeSparseTerms(dtm3, 0.9999)
+dtm3 <- removeSparseTerms(dtm3, 0.99995)
 mat3 <- as.matrix(dtm3)
 wordfreq3 <- colSums(mat3)
 wordfreq3 <- sort(wordfreq3, decreasing = TRUE)
--- predictionmodel.R
+++ predictionmodel.R
@@ -1,6 +1,21 @@
 # prediction model for next word 
 # We need wordfreq files from eda.R for running this
-# wordfreq is a numeric vector containing frequencies in decreasing orders 
+# wordfreq is a numeric vector containing frequencies in decreasing orders, so we convert
+library(dplyr)
+
+wordfreq <- as.data.frame(wordfreq)
+gram1 <- rownames(wordfreq)
+wordfreq <- wordfreq %>% mutate(gram1 = gram1)
+
+wordfreq2 <- as.data.frame(wordfreq2)
+gram2 <- rownames(wordfreq2)
+gram2 <- gram2 %>% strsplit(split = " ") %>% unlist() %>% matrix(byrow = TRUE, ncol=2)
+wordfreq2 <- wordfreq2 %>% mutate(gram2a = gram2[,1], gram2b = gram2[,2])
+
+wordfreq3 <- as.data.frame(wordfreq3)
+gram3 <- rownames(wordfreq3)
+gram3 <- gram3 %>% strsplit(split = " ") %>% unlist() %>% matrix(byrow = TRUE, ncol=3)
+wordfreq3 <- wordfreq3 %>% mutate(gram3a = gram3[,1], gram3b = gram3[,2], gram3c = gram3[,3])
 
 
 # Step 1 : cut off the n-grams to predict 
